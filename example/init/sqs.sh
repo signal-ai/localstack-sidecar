e29a1811bf943c6ccc74f2119a2fe834508730f9
edoardo tosca <edoardo.tosca@gmail.com>
2018-06-05 10:25:10 +0100

    et - updated example

--- example/init/sqs.sh
+++ example/init/sqs.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 
-aws --endpoint-url http://localstack:4576 sqs create-queue --queue-name prod-pipeline-vulcan-builder-in
-aws --endpoint-url http://localstack:4576 sqs create-queue --queue-name prod-pipeline-vulcan-topic-builder-in
+aws --endpoint-url http://localstack:4576 sqs create-queue --queue-name a-sample-queue-name
+
